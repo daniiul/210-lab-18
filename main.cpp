6d52a27273ec58ee6706a300d98f45e5ff793592
daniiul <dmalakhov2005@gmail.com>
2024-10-04 14:59:41 -0700

    created node struct

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include <string>
+#include <iomanip>
 using namespace std;
+
+struct Node
+{
+    double rating;
+    string comments;
+    Node *next;
+};
+
+Node* addNodeHead(Node *, double, string);
+Node* addNodeTail(Node *, double, string);
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    Node *head = nullptr;
+
+    cout << "Which linked list method should we use?" << endl;
+    cout << setw(10) << "[1] New nodes are added at the head of the linked list" << endl;
+    cout << setw(5) << "[1] New nodes are added at the tail of the linked list" << endl;
 }
