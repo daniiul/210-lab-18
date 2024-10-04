90912637e8235d0decf2e0990630bdeac5acbf7b
daniiul <dmalakhov2005@gmail.com>
2024-10-04 15:12:45 -0700

    added Node addition functions

--- main.cpp
+++ main.cpp
@@ -13,12 +13,36 @@ struct Node
 Node* addNodeHead(Node *, double, string);
 Node* addNodeTail(Node *, double, string);
 
-
 int main()
 {
     Node *head = nullptr;
 
+    int entry;
+
     cout << "Which linked list method should we use?" << endl;
-    cout << setw(10) << "[1] New nodes are added at the head of the linked list" << endl;
-    cout << setw(5) << "[1] New nodes are added at the tail of the linked list" << endl;
+    cout << setw(61) << "[1] New nodes are added at the head of the linked list" << endl;
+    cout << setw(61) << "[2] New nodes are added at the tail of the linked list" << endl;
+    cout << "       Choice: ";
+    cin >> entry;
 }
+
+Node* addNodeHead(Node * head, double rating, string comment)
+{
+    Node *newRating = new Node;
+    if (!head)
+    {
+        head = newRating;
+        newRating->next = nullptr;
+        newRating->rating = rating;
+        newRating->comment = comment;
+    }
+    else
+    {
+        newRating->next = head;
+        newRating->rating = rating;
+        newRating->comment = comment;
+        head = newRating;
+    }
+    return head;
+}
+
